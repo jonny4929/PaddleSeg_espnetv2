95ce285cb37261a02069b735961963ac6db640f0
jonny4929 <1420245698@qq.com>
2021-08-08 03:28:05 +0800

    commit

--- README.md
+++ README.md
@@ -1,183 +1,35 @@
-English | [简体中文](README_CN.md)
+# ESPNetv2-paddle
 
-# PaddleSeg
+ESPNetv2 implementation using paddlepaddle and paddleseg
 
-[![Build Status](https://travis-ci.org/PaddlePaddle/PaddleSeg.svg?branch=master)](https://travis-ci.org/PaddlePaddle/PaddleSeg)
-[![License](https://img.shields.io/badge/license-Apache%202-blue.svg)](LICENSE)
-[![Version](https://img.shields.io/github/release/PaddlePaddle/PaddleSeg.svg)](https://github.com/PaddlePaddle/PaddleSeg/releases)
-![python version](https://img.shields.io/badge/python-3.6+-orange.svg)
-![support os](https://img.shields.io/badge/os-linux%2C%20win%2C%20mac-yellow.svg)
+## environment
 
-## PaddleSeg has released the new version including the following features:
+python == 3.7
 
-* Our team won the AutoNUE@CVPR 2021 challenge, where the technical [report](https://bj.bcebos.com/paddleseg/docs/autonue21_presentation_PaddleSeg.pdf) and [source code](https://github.com/PaddlePaddle/PaddleSeg/tree/release/2.2/contrib/AutoNUE) are available.
-* We released an efficient interactive annotation tool for image segmentation, named [EISeg](https://github.com/PaddlePaddle/PaddleSeg/tree/release/2.2/contrib/EISeg).
-* We introduced [Panoptic-DeepLab](https://github.com/PaddlePaddle/PaddleSeg/tree/release/2.2/contrib/PanopticDeepLab), which is a proposal-free algorithm  for panoptic segmentation.
-* We provided an ultra-lightweight [portrait segmentation](https://github.com/PaddlePaddle/PaddleSeg/tree/release/2.2/contrib/HumanSeg) solution for the mobile devices and even the web
+Do not use python3.8. We find the following code not supported when using python3.8. For more details, please read the following [issue](https://github.com/PaddlePaddle/Paddle/issues/26174)
 
-## PaddleSeg Introduction
+    copy.deepcopy(Conv(features, features, 3, 1, groups=features))
 
-Welcome to PaddleSeg! PaddleSeg is an end-to-end image segmentation development kit developed based on [PaddlePaddle](https://www.paddlepaddle.org.cn), which covers a large number of high-quality segmentation models in different directions such as *high-performance* and *lightweight*. With the help of modular design, we provide two application methods: *Configuration Drive* and *API Calling*. So one can conveniently complete the entire image segmentation application from training to deployment through configuration calls or API calls.
+paddlepaddle >= 2.1
 
-* ### PaddleSeg provides four image segmentation capabilities: semantic segmentation, interactive segmentation, panoptic segmentation and Matting.
+## training tricks
 
-<div align="center">
-<img src="./docs/images/ability_EN.gif"  width = "2000" />  
-</div>
+The configs are put in benchmark/espnetv2_config. please use it in the following order.
 
+espnetv2_iter80k.yml $\Rightarrow$ espnetv2_lovasz_40k.yml $\Rightarrow$ espnetv2_finetune_40k.yml
 
----------------
+First, using espnetv2_iter80k.yml, we use a smaller image resolution for training (512 × 256
+for the CityScapes dataset). we use the backbone pretrained on imagenet-1k provided by the author of ESPNetv2.(As the conv2d function is different when stride > 1 and padding > 0 in paddle and torch, the pretrained model provided by the author, which is trained using torch, is not perfect for our implementation. Maybe using paddle to train a new backbone can further improve the performance)
 
- * ### PaddleSeg is widely used in autonomous driving, medical, quality inspection, inspection, entertainment and other scenarios.
+Then, using espnetv2_lovasz_40k.yml, we use the lovasz loss to solve the problem of the unbalanced labels.
 
-<div align="center">
-<img src="./docs/images/application_EN.gif"  width = "2000" />  
-</div>
+Finally, using the espnetv2_finetune_40k.yml, we use DiceLoss and BootstrappedCrossEntropyLoss to finetune.
 
+It cost about 30 hours to train the model using v100 provided by AIstudio
 
----------------
+## evaluate
 
+| model | backbone | miou | miou(ms+flip) | backbone_url | model_url |
+|-------|----------|------|---------------|--------------|-----------|
+|ESPNetv2_Seg|ESPNetv2|0.6745|0.6906|[百度网盘(提取码: 63tf)](https://pan.baidu.com/s/1HAqo9JTawwQtVlj2gPKY2g)|[百度网盘(提取码: 63tf)](https://pan.baidu.com/s/1HAqo9JTawwQtVlj2gPKY2g)|
 
-
-## Core Features
-
-* <img src="./docs/images/f1.png" width="20"/> **High Performance Model**: Based on the high-performance backbone trained by Baidu's self-developed [semi-supervised label knowledge distillation scheme (SSLD)](https://paddleclas.readthedocs.io/zh_CN/latest/advanced_tutorials/distillation/distillation.html#ssld), combined with the state of the art segmentation technology, we provides 50+ high-quality pre-training models, which are better than other open source implementations.
-
-* <img src="./docs/images/f2.png" width="20"/> **Modular Design**: PaddleSeg support 15+ mainstream *segmentation networks*, developers can start based on actual application scenarios and assemble diversified training configurations combined with modular design of *data enhancement strategies*, *backbone networks*, *loss functions* and other different components to meet different performance and accuracy requirements.
-
-* <img src="./docs/images/f3.png" width="20"/> **High Efficiency**: PaddleSeg provides multi-process asynchronous I/O, multi-card parallel training, evaluation and other acceleration strategies, combined with the memory optimization function of the PaddlePaddle, which can greatly reduce the training overhead of the segmentation model, all this allowing developers to lower cost and more efficiently train image segmentation model.
-
-* :heart:**You can go to  [Complete PaddleSeg Online Documentation Directory](https://paddleseg.readthedocs.io)  for more detailed documentation**:heart:
-
-## Technical Communication <img src="./docs/images/chat.png" width="30"/>
-
-* If you find any problems or have a suggestion with PaddleSeg, please send us issues through [GitHub Issues](https://github.com/PaddlePaddle/PaddleSeg/issues).
-* Welcome to Join PaddleSeg QQ Group
-<div align="center">
-<img src="./docs/images/QQ_chat.png"  width = "200" />  
-</div>
-
-## Model Instraction  <img src="./docs/images/model.png" width="20"/>
-
-[Model Zoo](./configs/)
-
-<div align="center">
-<img src="./docs/images/xingnengtu.png"    width = "700"/>  
-</div>
-
-
-## Dataset
-
-- [x] Cityscapes
-- [x] Pascal VOC
-- [x] ADE20K
-- [x] Pascal Context
-- [x] COCO stuff
-
-## Tutorials <img src="./docs/images/teach.png" width="30"/>
-
-* [Installation](./docs/install.md)
-* [Get Started](./docs/quick_start.md)
-*  Prepare Datasets
-   * [Preparation of Annotation Data](./docs/data/marker/marker.md)
-   * [Annotating Tutorial](./docs/data/transform/transform.md)
-   * [Custom Dataset](./docs/data/custom/data_prepare.md)
-
-*  Custom Software Development of PaddleSeg
-    * [Detailed Configuration File](./docs/design/use/use.md)
-    * [Create Your Own Model](./docs/design/create/add_new_model.md)
-* [Model Training](/docs/train/train.md)
-* [Model Evaluation](./docs/evaluation/evaluate/evaluate.md)
-* [Model Export](./docs/export/export/model_export.md)
-
-*  Model Deploy
-    * [Inference](./docs/deployment/inference/inference.md)
-    * [Lite](./docs/deployment/lite/lite.md)
-    * [Serving](./docs/deployment/serving/serving.md)
-    * [Web](./docs/deployment/web/web.md)
-* [Model Compression](./docs/slim/slim/slim.md)
-*  API Tutorial
-    * [API Documention](./docs/apis/README.md)
-    * [API Application](./docs/api_example.md)
-*  Description of Important Modules
-    * [Data Augmentation](./docs/module/data/data.md)
-    * [Loss Description](./docs/module/loss/losses_en.md)
-    * [Tricks](./docs/module/tricks/tricks.md)
-* Description of Classical Models
-    * [DeeplabV3](./docs/models/deeplabv3.md)
-    * [UNet](./docs/models/unet.md)
-    * [OCRNet](./docs/models/ocrnet.md)
-    * [Fast-SCNN](./docs/models/fascnn.md)
-* [PR Instruction](./docs/pr/pr/pr.md)
-* [FAQ](./docs/faq/faq/faq.md)
-
-## Installation
-
-#### step 1. Install PaddlePaddle
-
-System Requirements:
-* PaddlePaddle >= 2.0.0
-* Python >= 3.6+
-
-Highly recommend you install the GPU version of PaddlePaddle, due to large overhead of segmentation models, otherwise it could be out of memory while running the models. For more detailed installation tutorials, please refer to the official website of [PaddlePaddle](https://www.paddlepaddle.org.cn/install/quick?docurl=/documentation/docs/zh/2.0/install/)。
-
-
-#### step 2. Install PaddleSeg
-Support to construct a customized segmentation framework with *API Calling* method for flexible development.
-
-```shell
-pip install paddleseg
-```
-
-
-#### step 3. Download PaddleSeg Repo
-Support to complete the whole process segmentation application with *Configuration Drive* method, simple and fast.
-
-```shell
-git clone https://github.com/PaddlePaddle/PaddleSeg
-```
-
-#### step 4. Verify Installation
-Run the following command. If you can train normally, you have installed it successfully.
-
-```shell
-python train.py --config configs/quick_start/bisenet_optic_disc_512x512_1k.yml
-```
-
-
-## Practical Cases
-
-* [HumanSeg](./contrib/HumanSeg)
-* [Cityscapes SOTA](./contrib/CityscapesSOTA)
-* [PanopticSegmentation](./contrib/PanopticDeepLab)
-
-## Feedbacks and Contact
-* The dynamic version is still under development, if you find any issue or have an idea on new features, please don't hesitate to contact us via [GitHub Issues](https://github.com/PaddlePaddle/PaddleSeg/issues).
-* PaddleSeg User Group (QQ): 1004738029 or 850378321 or 793114768
-
-## Acknowledgement
-* Thanks [jm12138](https://github.com/jm12138) for contributing U<sup>2</sup>-Net.
-* Thanks [zjhellofss](https://github.com/zjhellofss) (Fu Shenshen) for contributing Attention U-Net, and Dice Loss.
-* Thanks [liuguoyu666](https://github.com/liguoyu666), [geoyee](https://github.com/geoyee) for contributing U-Net++ and U-Net3+.
-* Thanks [yazheng0307](https://github.com/yazheng0307) (LIU Zheng) for contributing quick-start document.
-
-## Citation
-If you find our project useful in your research, please consider citing:
-
-```latex
-@misc{liu2021paddleseg,
-      title={PaddleSeg: A High-Efficient Development Toolkit for Image Segmentation},
-      author={Yi Liu and Lutao Chu and Guowei Chen and Zewu Wu and Zeyu Chen and Baohua Lai and Yuying Hao},
-      year={2021},
-      eprint={2101.06175},
-      archivePrefix={arXiv},
-      primaryClass={cs.CV}
-}
-
-@misc{paddleseg2019,
-    title={PaddleSeg, End-to-end image segmentation kit based on PaddlePaddle},
-    author={PaddlePaddle Authors},
-    howpublished = {\url{https://github.com/PaddlePaddle/PaddleSeg}},
-    year={2019}
-}
-```
---
+++ paddleseg/models/espnet_v2.py
@@ -0,0 +1,389 @@
+import copy
+import math
+import paddle
+import paddle.nn as nn
+import paddle.nn.functional as F
+
+from paddleseg.cvlibs import manager, param_init
+from paddleseg import utils
+
+class ConvBNPRelu(nn.Layer):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        padding='same',
+        dilation=1,
+        groups=1
+        ):
+        super().__init__()
+        self.out_channels = out_channels
+        self.conv = nn.Conv2D(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+                dilation=dilation,
+                groups=groups,
+                bias_attr=False
+            )
+        self.bn = nn.BatchNorm2D(out_channels)
+        self.act = nn.PReLU(out_channels)
+
+    def forward(self, input):
+        '''
+        :param input: input feature map
+        :return: transformed feature map
+        '''
+        output = self.conv(input)
+        # output = self.conv1(output)
+        output = self.bn(output)
+        output = self.act(output)
+        return output
+
+class ConvBN(nn.Layer):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        padding='same',
+        dilation=1,
+        groups=1
+        ):
+        super().__init__()
+        self.out_channels = out_channels
+        self.conv = nn.Conv2D(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+                dilation=dilation,
+                groups=groups,
+                bias_attr=False
+            )
+        self.bn = nn.BatchNorm2D(out_channels)
+
+    def forward(self, input):
+        '''
+        :param input: input feature map
+        :return: transformed feature map
+        '''
+        output = self.conv(input)
+        # output = self.conv1(output)
+        output = self.bn(output)
+        return output
+
+
+class Conv(nn.Layer):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size,
+        stride=1,
+        padding='same',
+        dilation=1,
+        groups=1
+        ):
+        super().__init__()
+        self.out_channels = out_channels
+        self.conv = nn.Conv2D(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+                dilation=dilation,
+                groups=groups,
+                bias_attr=False
+            )
+
+    def forward(self, input):
+        '''
+        :param input: input feature map
+        :return: transformed feature map
+        '''
+        output = self.conv(input)
+        return output
+
+
+class PSPModule(nn.Layer):
+    def __init__(self, features, out_features=1024, sizes=(1, 2, 4, 8)):
+        super().__init__()
+        self.stages = nn.LayerList([
+            copy.deepcopy(Conv(features, features, 3, 1, groups=features))
+            for size in sizes])
+        self.project = ConvBNPRelu(features * (len(sizes) + 1), out_features, 1, 1)
+ 
+    def forward(self, feats):
+        h, w = feats.shape[2], feats.shape[3]
+        out = [feats]
+        for stage in self.stages:
+            feats = F.avg_pool2d(feats, kernel_size=3, stride=2, padding=1)
+            upsampled = F.interpolate(stage(feats), size=(h, w), mode='bilinear', align_corners=True)
+            out.append(upsampled)
+        return self.project(paddle.concat(out, axis=1))
+
+
+class EESP(nn.Layer):
+    def __init__(self, in_channels, out_channels, stride=1, k=4, r_lim=7, down_method='esp'):
+        super().__init__()
+        self.out_channels = out_channels
+        self.stride = stride
+        n = int(out_channels / k)
+        n1 = out_channels - (k - 1) * n
+        assert down_method in ['avg', 'esp'], 'One of these is suppported (avg or esp)'
+        assert n == n1, "n(={}) and n1(={}) should be equal for Depth-wise Convolution ".format(n, n1)
+        self.proj_1x1 = ConvBNPRelu(in_channels, n, 1, groups=k)
+        
+        map_receptive_ksize = {3: 1, 5: 2, 7: 3, 9: 4, 11: 5, 13: 6, 15: 7, 17: 8}
+        self.k_sizes = []
+        for i in range(k):
+            ksize = int(3 + 2 * i)
+            # After reaching the receptive field limit, fall back to the base kernel size of 3 with a dilation rate of 1
+            ksize = ksize if ksize <= r_lim else 3
+            self.k_sizes.append(ksize)
+        self.k_sizes.sort()
+
+        self.spp_dw = nn.LayerList()
+
+        for i in range(k):
+            d_rate = map_receptive_ksize[self.k_sizes[i]]
+            self.spp_dw.append(Conv(n, n, 3, stride=stride, groups=n, dilation=d_rate))
+
+        self.conv_1x1_exp = ConvBN(out_channels, out_channels, 1, 1,groups=k)
+        self.br_after_cat = nn.Sequential(
+            nn.BatchNorm2D(out_channels),
+            nn.PReLU(out_channels)
+        )
+        self.module_act = nn.PReLU(out_channels)
+        self.downAvg = True if down_method == 'avg' else False
+
+    def forward(self, x):
+
+        output1 = self.proj_1x1(x)
+        outputs = [self.spp_dw[0](output1)]
+        for k in range(1,len(self.spp_dw)):
+            out_k = self.spp_dw[k](output1)
+            out_k = out_k + outputs[-1]
+            outputs.append(out_k)
+        # Merge
+        expanded = self.conv_1x1_exp( # Aggregate the feature maps using point-wise convolution
+            self.br_after_cat( # apply batch normalization followed by activation function (PRelu in this case)
+                paddle.concat(outputs, 1) # concatenate the output of different branches
+            )
+        )
+        del outputs
+        # if down-sampling, then return the concatenated vector
+        # as Downsampling function will combine it with avg. pooled feature map and then threshold it
+        if self.stride == 2 and self.downAvg:
+            return expanded
+
+        # if dimensions of input and concatenated vector are the same, add them (RESIDUAL LINK)
+        if expanded.shape == x.shape:
+            expanded = expanded + x
+
+        # Threshold the feature map using activation function (PReLU in this case)
+        return self.module_act(expanded)
+
+
+class DownSampler(nn.Layer):
+    '''
+    Down-sampling fucntion that has two parallel branches: (1) avg pooling
+    and (2) EESP block with stride of 2. The output feature maps of these branches
+    are then concatenated and thresholded using an activation function (PReLU in our
+    case) to produce the final output.
+    '''
+
+    def __init__(self, in_channels, out_channels, k=4, r_lim=9, reinf=True):
+        '''
+            :param nin: number of input channels
+            :param nout: number of output channels
+            :param k: # of parallel branches
+            :param r_lim: A maximum value of receptive field allowed for EESP block
+            :param g: number of groups to be used in the feature map reduction step.
+        '''
+        super().__init__()
+        self.out_channels = out_channels
+        out_new = out_channels - in_channels
+        self.eesp = EESP(in_channels, out_new, stride=2, k=k, r_lim=r_lim, down_method='avg')
+        self.avg = nn.AvgPool2D(kernel_size=3, padding=1, stride=2)
+        if reinf:
+            self.inp_reinf = nn.Sequential(
+                ConvBNPRelu(config_inp_reinf, config_inp_reinf, 3, 1),
+                ConvBN(config_inp_reinf, out_channels, 1, 1)
+            )
+        self.act =  nn.PReLU(out_channels)
+
+    def forward(self, input, input2=None):
+        '''
+        :param input: input feature map
+        :return: feature map down-sampled by a factor of 2
+        '''
+        avg_out = self.avg(input)
+        eesp_out = self.eesp(input)
+        output = paddle.concat([avg_out, eesp_out], 1)
+        if input2 is not None:
+            #assuming the input is a square image
+            w1 = avg_out.shape[2]
+            while True:
+                input2 = F.avg_pool2d(input2, kernel_size=3, padding=1, stride=2)
+                w2 = input2.shape[2]
+                if w2 == w1:
+                    break
+            output = output + self.inp_reinf(input2)
+
+        return self.act(output) #self.act(output)
+
+
+class EESPNet(nn.Layer):
+    '''
+    the ESPNetv2 implementation for backbone
+    '''
+
+    def __init__(self, s=1):
+        '''
+        :param classes: number of classes in the dataset. Default is 20 for the cityscapes
+        :param s: factor that scales the number of output feature maps
+        '''
+        super().__init__()
+        reps = [0, 3, 7, 3]  # how many times EESP blocks should be repeated.
+        channels = 3
+
+        r_lim = [13, 11, 9, 7, 5]  # receptive field at each spatial level
+        K = [4]*len(r_lim) # No. of parallel branches at different levels
+
+        base = 32 #base configuration
+        config_len = 5
+        config = [base] * config_len
+        base_s = 0
+        for i in range(config_len):
+            if i== 0:
+                base_s = int(base * s)
+                base_s = math.ceil(base_s / K[0]) * K[0]
+                config[i] = base if base_s > base else base_s
+            else:
+                config[i] = base_s * pow(2, i)
+        if s <= 1.5:
+            config.append(1024)
+        elif s in [1.5, 2]:
+            config.append(1280)
+        else:
+            ValueError('Configuration not supported')
+
+        #print('Config: ', config)
+
+        global config_inp_reinf
+        config_inp_reinf = 3
+        self.input_reinforcement = True
+        assert len(K) == len(r_lim), 'Length of branching factor array and receptive field array should be the same.'
+        
+        self.level1 = ConvBNPRelu(channels, config[0], 3, 2)  # 112 L1
+
+        self.level2_0 = DownSampler(config[0], config[1], k=K[0], r_lim=r_lim[0], reinf=self.input_reinforcement)  # out = 56
+        self.level3_0 = DownSampler(config[1], config[2], k=K[1], r_lim=r_lim[1], reinf=self.input_reinforcement) # out = 28
+        self.level3 = nn.LayerList([
+            copy.deepcopy(EESP(config[2], config[2], stride=1, k=K[2], r_lim=r_lim[2]))
+            for _ in range(reps[1])
+        ])
+
+        self.level4_0 = DownSampler(config[2], config[3], k=K[2], r_lim=r_lim[2], reinf=self.input_reinforcement) #out = 14
+        self.level4 = nn.LayerList()
+        self.level4 = nn.LayerList([
+            copy.deepcopy(EESP(config[3], config[3], stride=1, k=K[3], r_lim=r_lim[3]))
+            for _ in range(reps[2])
+        ])
+
+    def forward(self, input, p=0.2, seg=True):
+        '''
+        :param input: Receives the input RGB image
+        :return: a C-dimensional vector, C=# of classes
+        '''
+        out_l1 = self.level1(input)  # 112
+        if not self.input_reinforcement:
+            del input
+            input = None
+
+        out_l2 = self.level2_0(out_l1, input)  # 56
+
+        out_l3_0 = self.level3_0(out_l2, input)  # out_l2_inp_rein
+        for i, layer in enumerate(self.level3):
+            if i == 0:
+                out_l3 = layer(out_l3_0)
+            else:
+                out_l3 = layer(out_l3)
+
+        out_l4_0 = self.level4_0(out_l3, input)  # down-sampled
+        for i, layer in enumerate(self.level4):
+            if i == 0:
+                out_l4 = layer(out_l4_0)
+            else:
+                out_l4 = layer(out_l4)
+        return out_l1, out_l2, out_l3, out_l4
+
+
+@manager.MODELS.add_component
+class EESPNet_Seg(nn.Layer):
+    """
+    espnetv2 implementation for segmentation
+    """
+    def __init__(self, num_classes=20, s=1, pretrained=None, gpus=1):
+        super().__init__()
+        self.pretrained = pretrained
+        self.net = EESPNet(s=s)
+        self.net.set_state_dict(paddle.load('./espnetv2_backbone.pdparams'))
+        if s <=0.5:
+            p = 0.1
+        else:
+            p=0.2
+
+        self.proj_L4_C = ConvBNPRelu(self.net.level4[-1].out_channels, self.net.level3[-1].out_channels, 1, 1)
+        pspSize = 2*self.net.level3[-1].out_channels
+        self.pspMod = nn.Sequential(EESP(pspSize, pspSize //2, stride=1, k=4, r_lim=7),
+                PSPModule(pspSize // 2, pspSize //2))
+        self.project_l3 = nn.Sequential(
+            nn.Dropout2D(p=p),
+            Conv(pspSize // 2, num_classes, 1, 1)
+        )
+        self.act_l3 = nn.Sequential(
+            nn.BatchNorm2D(num_classes),
+            nn.PReLU(num_classes)
+        )
+        self.project_l2 = ConvBNPRelu(self.net.level2_0.out_channels + num_classes, num_classes, 1, 1)
+        self.project_l1 = nn.Sequential(
+            nn.Dropout2D(p=p),
+            Conv(self.net.level1.out_channels + num_classes, num_classes, 1, 1)
+        )
+        self.init_weight()
+
+    def init_weight(self):
+        if self.pretrained is not None:
+            utils.load_entire_model(self, self.pretrained)
+
+    def hierarchicalUpsample(self, x, factor=3):
+        for i in range(factor):
+            x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=True)
+        return x
+
+
+    def forward(self, input):
+        out_l1, out_l2, out_l3, out_l4 = self.net(input, seg=True)
+        out_l4_proj = self.proj_L4_C(out_l4)
+        up_l4_to_l3 = F.interpolate(out_l4_proj, scale_factor=2, mode='bilinear', align_corners=True)
+        merged_l3_upl4 = self.pspMod(paddle.concat([out_l3, up_l4_to_l3], 1))
+        proj_merge_l3_bef_act = self.project_l3(merged_l3_upl4)
+        proj_merge_l3 = self.act_l3(proj_merge_l3_bef_act)
+        out_up_l3 = F.interpolate(proj_merge_l3, scale_factor=2, mode='bilinear', align_corners=True)
+        merge_l2 = self.project_l2(paddle.concat([out_l2, out_up_l3], 1))
+        out_up_l2 = F.interpolate(merge_l2, scale_factor=2, mode='bilinear', align_corners=True)
+        merge_l1 = self.project_l1(paddle.concat([out_l1, out_up_l2], 1))
+        if self.training:
+            return F.interpolate(merge_l1, scale_factor=2, mode='bilinear', align_corners=True), self.hierarchicalUpsample(proj_merge_l3_bef_act)
+        else:
+            return [F.interpolate(merge_l1, scale_factor=2, mode='bilinear', align_corners=True)]
